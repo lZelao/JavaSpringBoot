ffe9e9e9add578d57104b12b45f21e56219f7518
Eliseu <esreis123@gmail.com>
2024-01-24 01:52:43 -0300

    Injeçao de Dependencia

---
+++ VendasApp/src/main/java/com/example/VendasApp/repository/ClientRepository.java
@@ -0,0 +1,10 @@
+package com.example.VendasApp.repository;
+
+import com.example.VendasApp.model.Client;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientRepository {
+    public void persistir(Client client) {
+    }
+}
